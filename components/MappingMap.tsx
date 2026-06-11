a6f94642f96196ce887988bb56dca73a69376aad
n3rxv <nirxv03@gmail.com>
2026-06-11 14:01:23 +0530

    Switch mapping page to light tiles

--- components/MappingMap.tsx
+++ components/MappingMap.tsx
@@ -57,7 +57,7 @@ export default function MappingMap({
         attributionControl={false}
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution="&copy; OpenStreetMap &copy; CARTO"
         />
         <FitBounds sites={validSites} />
